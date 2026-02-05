ac12f84fce0fa5e24c051bbbe87029a15621a588
Monnelisa <monnelisa01mangalie@gmail.com>
2026-02-05 16:31:02 +0200

    update

--- Core/DriverFactory.cs
+++ Core/DriverFactory.cs
@@ -2,6 +2,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Serilog;
 using System;
+using System.Net;
 using TakealotAutomation.Configuration;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -31,13 +32,28 @@ namespace TakealotAutomation.Core
 
         private static IWebDriver InitializeChromeDriver()
         {
-            new DriverManager().SetUpDriver(new ChromeConfig());
+            try
+            {
+                // Try to setup driver using WebDriverManager (for environments with internet)
+                new DriverManager().SetUpDriver(new ChromeConfig());
+                Log.Information("ChromeDriver configured via WebDriverManager");
+            }
+            catch (WebException)
+            {
+                Log.Information("Network unavailable, using bundled ChromeDriver from NuGet package");
+            }
+            catch (Exception ex)
+            {
+                Log.Information($"WebDriverManager unavailable ({ex.GetType().Name}), using bundled ChromeDriver");
+            }
 
             var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
             options.AddArgument("--disable-notifications");
             options.AddArgument("--disable-popup-blocking");
             options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--disable-plugins");
             options.AddExcludedArgument("enable-automation");
 
             if (ConfigurationManager.IsHeadlessMode())
